e33ad65f43ec78a3fc8ad602978f6bb52cd51c6f
rop-lab <jhnycheru11@gmail.com>
2023-12-30 19:12:29 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution here!
+let cats =["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+beforeEach(function() {
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+});
